c94b6801fd16ddb92f29a645f9168cd606984135
Eugene <viiper94@gmail.com>
2017-08-11 15:55:40 +0300

    update footer

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -194,23 +194,63 @@ AppAsset::register($this);
     <footer class="page-footer grey lighten-3 ">
         <div class="container">
             <div class="row flex">
-                <div class="footer-left">
-                    <h6 class="black-text">МЫ В СОЦИАЛЬНЫХ СЕТЯХ</h6>
-                    <ul class="socials links">
-                        <li class="vk z-depth-3"><a class="waves-effect waves-light" target="_blank" href="https://vk.com/volvo_trucks_russia"></a></li>
-                        <li class="steam z-depth-3"><a class="waves-effect waves-light" target="_blank" href="http://steamcommunity.com/groups/volvo_trucks"></a></li>
-                        <li class="instagram z-depth-3"><a class="waves-effect waves-light" target="_blank" href="https://instagram.com/volvo_trucks_russia"></a></li>
-                        <li class="youtube z-depth-3"><a class="waves-effect waves-light" target="_blank" href="https://www.youtube.com/channel/UCCUkLXBObH0IA54XhCaDTzg"></a></li>
-                    </ul>
+                <div class="footer-left black-text">
+                    <div class="col m4 s12">
+                        <p><i class="material-icons notranslate left">featured_play_list</i>ВАКАНСИИ</p>
+                        <ul>
+                            <li><a href="<?= Url::to(['site/recruit']) ?>">Вступить в ВТК</a></li>
+                            <li><a href="<?= Url::to(['site/members', 'action' => 'stat']) ?>">Список сотрудников</a></li>
+                        </ul>
+                        <p><i class="material-icons notranslate left">note_add</i>ЗАЯВЛЕНИЯ</p>
+                        <ul>
+                            <li><a href="<?= Url::to(['site/claims']) ?>">Написать заявление</a></li>
+                            <li><a href="#">Жалоба на водителя</a></li>
+                        </ul>
+                    </div>
+                    <div class="col m4 s12">
+                        <p><i class="material-icons notranslate left">error</i>ПРАВИЛА</p>
+                        <ul>
+                            <li><a href="<?= Url::to(['site/rules']) ?>">Правила компании</a></li>
+                            <li><a href="https://forum.truckersmp.com/index.php?/topic/5807-in-game-rules-rus-игровые-правила-08-06-2017/" target="_blank">Правила TruckersMP</a></li>
+                        </ul>
+                        <p><i class="material-icons notranslate left">settings</i>МОДИФИКАЦИИ</p>
+                        <ul>
+                            <li><a href="<?= Url::to(['site/mods']) ?>">Моды для ETS2MP</a></li>
+                            <li><a href="<?= Url::to(['site/mods']) ?>">Моды для ATSMP</a></li>
+                        </ul>
+                    </div>
+                    <div class="col m4 s12">
+                        <p><i class="material-icons notranslate left">mic</i>СВЯЗЬ</p>
+                        <ul>
+                            <li><a href="https://www.teamspeak.com/en/downloads" target="_blank">Скачать TeamSpeak3</a></li>
+                            <li><a href="http://teamspeak-ru.ru/rusifikator-dlya-teamspeak-3.html" target="_blank">Скачать русификатор</a></li>
+                        </ul>
+                        <p><i class="material-icons notranslate left">lightbulb_outline</i>ФИРМЕННЫЙ СТИЛЬ</p>
+                        <ul>
+                            <li><a href="<?= Url::to(['site/variations', 'game' => 'ets2']) ?>">Отделение ETS2</a></li>
+                            <li><a href="<?= Url::to(['site/variations', 'game' => 'ats']) ?>">Отделение ATS</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="footer-right">
-                    <h6 class="black-text">ПОЛЕЗНЫЕ ССЫЛКИ</h6>
-                    <ul class="links adds">
-                        <li class="truckersmp"><a class="waves-effect" target="_blank" href="https://truckersmp.com/"></a></li>
-                        <li class="scs"><a class="waves-effect" target="_blank" href="http://blog.scssoft.com/"></a></li>
-                        <li class="ets"><a class="waves-effect" target="_blank" href="https://www.eurotrucksimulator2.com/"></a></li>
-                        <li class="ats"><a class="waves-effect" target="_blank" href="http://www.americantrucksimulator.com/"></a></li>
-                    </ul>
+                <div class="footer-right right-align">
+                    <div class="footer-top">
+                        <h6 class="black-text">МЫ В СОЦИАЛЬНЫХ СЕТЯХ</h6>
+                        <ul class="socials links">
+                            <li class="vk z-depth-3"><a class="waves-effect waves-light" target="_blank" href="https://vk.com/volvo_trucks_russia"></a></li>
+                            <li class="steam z-depth-3"><a class="waves-effect waves-light" target="_blank" href="http://steamcommunity.com/groups/volvo_trucks"></a></li>
+                            <li class="instagram z-depth-3"><a class="waves-effect waves-light" target="_blank" href="https://instagram.com/volvo_trucks_russia"></a></li>
+                            <li class="youtube z-depth-3"><a class="waves-effect waves-light" target="_blank" href="https://www.youtube.com/channel/UCCUkLXBObH0IA54XhCaDTzg"></a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-bottom">
+                        <h6 class="black-text">ПОЛЕЗНЫЕ ССЫЛКИ</h6>
+                        <ul class="links adds">
+                            <li class="truckersmp"><a class="waves-effect" target="_blank" href="https://truckersmp.com/"></a></li>
+                            <li class="scs"><a class="waves-effect" target="_blank" href="http://blog.scssoft.com/"></a></li>
+                            <li class="ets"><a class="waves-effect" target="_blank" href="https://www.eurotrucksimulator2.com/"></a></li>
+                            <li class="ats"><a class="waves-effect" target="_blank" href="http://www.americantrucksimulator.com/"></a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
