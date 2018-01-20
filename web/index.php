ea883b1694ea8ae248d958e8267cd46eceba8d60
viiper94 <viiper94@gmail.com>
2018-01-20 14:49:00 +0200

    clean up

--- web/index.php
+++ web/index.php
@@ -7,7 +7,6 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require(__DIR__ . '/../lib/kint/Kint.class.php');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
-require(__DIR__ . '/../lib/zebra/Zebra_Image.php');
 
 $config = require(__DIR__ . '/../config/web.php');
 
