31cbaec97f9521b14a2614b98fbd27664c220065
viiper94 <viiper94@gmail.com>
2018-02-20 11:51:11 +0200

    some restylings

--- views/modifications/index.php
+++ views/modifications/index.php
@@ -103,25 +103,21 @@ $this->registerMetaTag([
         <div class="card-image tedit-img hide-on-med-and-down" style="background-image: url(<?=Yii::$app->request->baseUrl?>/assets/img/mods/tedit.png)"></div>
         <div class="card-stacked">
             <div class="card-content">
-                <span class="card-title">Личный прицеп</span>
+                <span class="card-title">TEDIT - Личный прицеп</span>
+				<p><i class="material-icons notranslate left orange-text">warning</i>Для корректной работы программы нужно открыть <b>config.cfg</b> через текстовый редактор и
+					в параметре <b>g_save_format</b> изменить значение <b>0</b> на <b>2</b>.</p>
                 <ul class="collapsible" data-collapsible="accordion">
                     <li>
-                        <div class="collapsible-header grey lighten-4"><i class="material-icons">ondemand_video</i>Euro Truck Simulator 2</div>
+                        <div class="collapsible-header grey lighten-4"><i class="material-icons notranslate">ondemand_video</i>Euro Truck Simulator 2</div>
                         <div class="collapsible-body">
-                            <p>Для корректной работы программы нужно открыть <b>config.cfg</b> через текстовый редактор и
-                                в параметре <b>g_save_format</b> изменить значение <b>0</b> на <b>2</b>.</p>
-                            <h6><a href="<?= Yii::$app->request->baseUrl ?>/tedit552_ets.zip" target="_blank">Скачать TEDIT v5.5.2</a></h6>
                             <div class="video-container">
                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/XOYwF48E_PU?rel=0" frameborder="0" allowfullscreen></iframe>
                             </div>
                         </div>
                     </li>
                     <li>
-                        <div class="collapsible-header grey lighten-4"><i class="material-icons">ondemand_video</i>American Truck Simulator</div>
+                        <div class="collapsible-header grey lighten-4"><i class="material-icons notranslate">ondemand_video</i>American Truck Simulator</div>
                         <div class="collapsible-body">
-                            <p>Для корректной работы программы нужно открыть <b>config.cfg</b> через текстовый редактор и
-                                в параметре <b>g_save_format</b> изменить значение <b>0</b> на <b>2</b>.</p>
-                            <h6><a href="<?= Yii::$app->request->baseUrl ?>/tedit552_ats.zip" target="_blank">Скачать TEDIT v5.5.2</a></h6>
                             <div class="video-container">
                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/TwS63ASk36E?rel=0" frameborder="0" allowfullscreen></iframe>
                             </div>
@@ -129,6 +125,15 @@ $this->registerMetaTag([
                     </li>
                 </ul>
             </div>
+			<div class="card-action">
+				<a class="btn indigo darken-3 waves-effect waves-light" href="<?= Yii::$app->request->baseUrl ?>/tedit_v6.11.2.rar" target="_blank">
+					<i class="material-icons notranslate left">file_download</i>
+					Скачать TEDIT v6.11.2
+				</a>
+				<a href="http://truck-sim.club/topic/21801-svoi-pritcep-tedit-v6112-reliz-i-obschee-obsuzhdenie/?p=371683" class="right btn-flat" target="_blank">
+					Всегда свежая версия
+				</a>
+			</div>
         </div>
     </div>
 </div>
