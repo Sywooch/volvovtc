962510fb6152f6b0ddcb2abe3a50088be760b380
viiper94 <viiper94@gmail.com>
2018-02-26 15:41:49 +0200

    new layout

--- views/convoys/edit_convoy.php
+++ views/convoys/edit_convoy.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 if(!$model->game) $model->game = Yii::$app->request->get('game');
-$this->title = Yii::$app->controller->action->id == 'add' ? 'Добавить конвой' : 'Редактировать конвой' ;
+$this->title = Yii::$app->controller->action->id == 'add' ? 'Добавить конвой' : 'Редактирование конвоя' ;
 $this->title .= $model->game == 'ets' ? ' по ETS2' : ' по ATS';
 $this->title .= ' - Volvo Trucks';
 $this->registerJsFile(Yii::$app->request->baseUrl.'/assets/js/cities.js?t='.time(),  ['position' => yii\web\View::POS_END]);
@@ -17,19 +17,37 @@ $this->registerJsFile(Yii::$app->request->baseUrl.'/lib/ck-editor/ckeditor.js?t=
 ?>
 
 <div class="container">
-    <h5 class="light"><?= str_replace(' - Volvo Trucks', '', $this->title) ?></h5>
+	<div class="row" style="margin-top: 20px;">
+		<?php if(Yii::$app->controller->action->id == 'add') : ?>
+			<div class="col m6 s12 right-align">
+				<a href="<?= Url::to(['convoys/add', 'game' => 'ets']) ?>" class="btn-flat waves-effect<?php if($model->game == 'ets'): ?> disabled<?php endif ?>">
+					Добавить конвой по ETS2</a>
+			</div>
+			<div class="col m6 s12">
+				<a href="<?= Url::to(['convoys/add', 'game' => 'ats']) ?>" class="btn-flat waves-effect<?php if($model->game == 'ats'): ?> disabled<?php endif ?>">
+					Добавить конвой по ATS</a>
+			</div>
+		<?php else: ?>
+			<h5 class="col s12">
+				Редактирование конвоя:
+				<a href="<?= Url::to(['convoys/index', 'id' => Yii::$app->request->get('id')]) ?>">
+					<?= $model->title ?> от <?= Yii::$app->formatter->asDate($model->departure_time, 'php:j F Y') ?>
+				</a>
+			</h5>
+		<?php endif ?>
+	</div>
     <div class="row">
         <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+		<div class="col s12"><h5 class="light">Карта маршрута</h5></div>
         <div class="col m6 s12">
             <div class="card-panel grey lighten-4">
-                <h5 class="light">Основная информация</h5>
 				<div class="picture-full">
 					<label>Оригинальная карта маршрута (макс. 15 Мб)</label>
 					<div class="file-field">
 						<div class="btn indigo darken-3 waves-effect waves-light">
 							<span>Загрузить изображение</span>
 							<?= $form->field($model, 'picture_full')->fileInput([
-								'class' => 'validate-img-size',
+								'class' => 'convoy-validate-img-size',
 								'data-maxsize' => '15000000',
 								'data-alert' => 'Максимальный размер файла 15Мб',
 							]) ?>
@@ -99,22 +117,28 @@ $this->registerJsFile(Yii::$app->request->baseUrl.'/lib/ck-editor/ckeditor.js?t=
             </div>
         </div>
         <div class="col m6 s12">
-            <div class="card-panel grey lighten-4">
-                <h5 class="light">Карта маршрута</h5>
-                <?php if($model->picture_small): ?>
-                    <img src="<?=Yii::$app->request->baseUrl?>/images/convoys/<?= $model->picture_small ?>?t=<?= time() ?>" class="responsive-img z-depth-2" id="preview">
-                    <?= $form->field($model, 'map_remove', [
-                        'template' => '{input}{label}',
-                        'options' => ['tag' => false]])->checkbox(['label' => null])->error(false)->label('Удалить карту маршрута') ?>
-                <?php else: ?>
-                    <img src="<?=Yii::$app->request->baseUrl?>/assets/img/no_route.jpg" class="responsive-img z-depth-2" id="preview">
-                <?php endif ?>
+            <div class="card grey lighten-4">
+				<div class="card-image" style="max-height: 310px;">
+					<?php if($model->picture_small): ?>
+						<img src="<?=Yii::$app->request->baseUrl?>/images/convoys/<?= $model->picture_small ?>?t=<?= time() ?>"
+							 class="responsive-img" id="preview">
+					</div>
+					<div class="card-action">
+						<a href="<?= Url::to(['convoys/deletemap', 'id' => Yii::$app->request->get('id')]) ?>"
+							onclick="return confirm('Карта будет удалена?')">
+							<i class="material-icons notranslate left">delete</i>
+							Удалить карту маршрута
+						</a>
+					<?php else: ?>
+						<img src="<?=Yii::$app->request->baseUrl?>/assets/img/no_route.jpg" class="responsive-img" id="preview">
+					<?php endif ?>
+				</div>
             </div>
         </div>
         <div class="clearfix"></div>
         <div class="col l6 s12">
+			<h5 class="light">Сборы</h5>
             <div class="card-panel grey lighten-4">
-                <h5 class="light">Сборы</h5>
                 <?= $form->field($model, 'date')
                     ->input('date', [
                         'class' => 'datepicker-convoy',
@@ -130,8 +154,8 @@ $this->registerJsFile(Yii::$app->request->baseUrl.'/lib/ck-editor/ckeditor.js?t=
             </div>
         </div>
         <div class="col l6 s12">
+			<h5 class="light">Маршрут</h5>
             <div class="card-panel grey lighten-4">
-                <h5 class="light">Маршрут</h5>
                 <?php $class = 'autocomplete';
                 if($model->game == 'ats') $class .= '-ats'; ?>
                 <div class="input-field">
@@ -154,10 +178,10 @@ $this->registerJsFile(Yii::$app->request->baseUrl.'/lib/ck-editor/ckeditor.js?t=
                 </div>
             </div>
         </div>
-        <div class="clearfix"></div>
+        <div class="clearfix col s12"><h5 class="light">Основная информация</h5></div>
         <div class="col l6 s12">
-            <div class="card-panel grey lighten-4">
-                <h5 class="light" style="padding-bottom: 10px">Дополнительная информация</h5>
+
+			<div class="card-panel grey lighten-4">
                 <div class="input-field">
                     <?= $form->field($model, 'truck_var', ['template' => '{input}{label}'])
                         ->dropdownList(\app\models\Convoys::getVariationsByGame($model->game))
@@ -167,36 +191,23 @@ $this->registerJsFile(Yii::$app->request->baseUrl.'/lib/ck-editor/ckeditor.js?t=
                     <?= $form->field($model, 'attach_var_photo', ['template' => '<div>{input}{label}</div>'])
                         ->checkbox(['label' => null])->label('Прикрепить фото вариации') ?>
                 <?php endif ?>
-                <?php foreach($model->trailer as $key => $trailer) : ?>
-                    <div class="row inner">
-                        <div class="col l11 s10" style="padding-bottom: 20px;">
-                            <?php if($key == 0) : ?><label class="control-label">Прицепы</label><?php endif ?>
-                            <?= $form->field($model, 'trailer['.$key.']')
-                                ->dropdownList($trailers, [
-                                    'id' => 'trailer-select-'.$key,
-                                    'class' => 'browser-default trailers-select',
-                                    'data-target' => 'trailers'])
-                                ->error(false)
-                                ->label(false) ?>
-                            <script>
-                                $('#trailer-select-'+<?=$key?>).select2();
-                            </script>
-                        </div>
-                        <?php if($key == 0) : ?>
-                            <div class="col l1 s2 center add-btn-container" style="line-height: 66px;">
-                                <button class="tooltipped indigo-text add-trailer" data-position="bottom" data-tooltip="Добавить прицеп">
-                                    <i class="material-icons notranslate small">add</i>
-                                </button>
-							</div>
-                        <?php else : ?>
-                            <div class="col l1 s2 center" style="line-height: 44px;">
-                                <button class="tooltipped red-text remove-trailer" data-position="bottom" data-tooltip="Убрать прицеп">
-                                    <i class="material-icons notranslate small">clear</i>
-                                </button>
-							</div>
-                        <?php endif ?>
-                    </div>
-                <?php endforeach ?>
+				<div class="row">
+					<div class="col l11 s10" style="padding-bottom: 20px;">
+						<label class="control-label">Прицеп на конвой</label>
+						<?= $form->field($model, 'trailer')
+							->dropdownList($trailers, [
+								'id' => 'trailer-select',
+								'class' => 'browser-default trailers-select',
+								'data-target' => 'trailers'])
+							->error(false)
+							->label(false) ?>
+					</div>
+					<div class="col l1 s2 center add-btn-container" style="line-height: 66px;">
+						<a href="<?= Url::to(['trailers/add']) ?>" class="tooltipped indigo-text" data-position="bottom" data-tooltip="Добавить новый прицеп">
+							<i class="material-icons notranslate small">add</i>
+						</a>
+					</div>
+				</div>
                 <div class="input-field">
                     <?= $form->field($model, 'author')->textInput() ?>
                 </div>
@@ -207,64 +218,61 @@ $this->registerJsFile(Yii::$app->request->baseUrl.'/lib/ck-editor/ckeditor.js?t=
             </div>
         </div>
         <div class="col l6 s12">
-            <div class="card-panel grey lighten-4">
-                <div id="trailer-info" class="row">
-                    <?php switch(count($model->trailer)){
-                        case '4' : $cols = 6;break;
-                        case '3' : $cols = 4;break;
-                        case '2' : $cols = 6;break;
-                        case '1' :
-                        default: $cols = 12;break;
-                    } ?>
-                    <?php if(Yii::$app->controller->action->id == 'edit'){
-                        foreach($model->trailer as $key => $trailer) : ?>
-                            <div class="trailer-preview col s<?=$cols?>">
-                                <img src="<?= Yii::$app->request->baseUrl . '/images/' . $trailers_data[$key] ?>" class="responsive-img z-depth-2 materialboxed" id="trailer-image-<?= $key ?>">
-                            </div>
-                        <?php endforeach;
-                    }else{ ?>
-                        <div class="trailer-preview col s<?=$cols?>">
-                            <img src="<?= Yii::$app->request->baseUrl . '/images/trailers/default.jpg' ?>" class="responsive-img z-depth-2 materialboxed" id="trailer-image-0">
-                        </div>
-                    <?php } ?>
-                    <div class="clearfix"></div>
-                </div>
-                <?php if($model->extra_picture) : ?>
-                    <img src="<?= Yii::$app->request->baseUrl . '/images/convoys/' . $model->extra_picture ?>" class="responsive-img z-depth-2">
-                    <a href="<?= Url::to(['convoys/deleteextrapicture', 'id' => $_GET['id']]) ?>" class="btn indigo darken-3 waves-effect waves-light" onclick="return confirm('Удалить дополнительное изображение?')">
-                        <i class="material-icons notranslate left">clear</i>Удалить
-                    </a>
-					<div class="clearfix"></div>
-                <?php endif ?>
-				<label>Дополнительное изображение (макс. 15 Мб)</label>
-                <div class="file-field">
-                    <div class="btn indigo darken-3 waves-effect waves-light">
-                        <i class="material-icons notranslate left">add</i>
-                        <span>Загрузить</span>
-                        <?= $form->field($model, 'extra_picture')->fileInput()->label(false)->error(false) ?>
-                    </div>
-                    <div class="file-path-wrapper">
-                        <input class="file-path" type="text" value="<?= $model->extra_picture ?>" readonly="readonly">
-                    </div>
-                </div>
-            </div>
-        </div>
-		<div class="col s12">
-			<div class="card-panel grey lighten-4">
-				<h5 class="light">Описание</h5>
-				<div class="input-field file-field">
-					<?= $form->field($model, 'add_info')->textarea(['class' => 'materialize-textarea', 'id' => 'add_info'])->label(false) ?>
+			<div class="card trailer-info">
+				<div class="card-image" style="max-height: 372px;" id="trailer-info">
+					<img src="<?= Yii::$app->request->baseUrl . '/images/trailers/' . ($model->tr_image ? $model->tr_image : 'default.jpg') ?>"
+						 class="responsive-img z-depth-2" id="trailer-image">
 				</div>
 			</div>
 		</div>
+		<div class="col s12 clearfix">
+			<h5 class="light">Дополнительная информация</h5>
+		</div>
+		<div class="col l4 s12">
+			<div class="card grey lighten-4">
+				<?php if($model->extra_picture) : ?>
+					<div class="card-image" style="max-height: 300px">
+						<img src="<?= Yii::$app->request->baseUrl . '/images/convoys/' . $model->extra_picture ?>" class="responsive-img z-depth-2">
+					</div>
+				<?php endif ?>
+					<div class="card-content">
+						<label>Дополнительное изображение (макс. 15 Мб)</label>
+						<div class="file-field">
+							<div class="btn indigo darken-3 waves-effect waves-light">
+								<i class="material-icons notranslate left">add</i>
+								<span>Загрузить</span>
+								<?= $form->field($model, 'extra_picture')->fileInput()->label(false)->error(false) ?>
+							</div>
+							<div class="file-path-wrapper">
+								<input class="file-path" type="text" value="<?= $model->extra_picture ?>" readonly="readonly">
+							</div>
+						</div>
+					</div>
+				<?php if($model->extra_picture) : ?>
+					<div class="card-action">
+						<a href="<?= Url::to(['convoys/deleteextrapicture', 'id' => Yii::$app->request->get('id')]) ?>"
+						   onclick="return confirm('Удалить дополнительное изображение?')">
+							<i class="material-icons notranslate left">delete</i>Удалить дополнительное изображение
+						</a>
+					</div>
+
+					<div class="clearfix"></div>
+				<?php endif ?>
+			</div>
+		</div>
+		<div class="col l8 s12" style="padding-top: .5em; margin-bottom: 15px;">
+			<?= $form->field($model, 'add_info')->textarea(['class' => 'materialize-textarea', 'id' => 'add_info'])->label(false) ?>
+		</div>
+		<div class="clearfix"></div>
         <div class="fixed-action-btn">
             <?=Html::submitButton(Html::tag('i', 'save', [
                     'class' => 'large material-icons notranslate'
-            ]), ['class' => 'btn-floating btn-large red']) ?>
+            ]), ['class' => 'btn-floating btn-large red waves-effect waves-light']) ?>
         </div>
         <?php ActiveForm::end(); ?>
     </div>
 </div>
 <script type="text/javascript">
     CKEDITOR.replace('add_info');
+	$('#trailer-select').select2();
 </script>
