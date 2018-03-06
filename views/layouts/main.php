c60a066f6ba65376b373ab163cbbe01806b2c5a6
viiper94 <viiper94@gmail.com>
2018-03-06 16:46:29 +0200

    hotfix

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -176,7 +176,7 @@ AppAsset::register($this);
                     </li>
                     <li<?php if(Yii::$app->controller->id === 'convoys'){?> class="active"<?php } ?>><a href="<?=Url::to(['convoys/index'])?>">КОНВОИ</a></li>
                     <li<?php if(Yii::$app->controller->id === 'gallery'){?> class="active"<?php } ?>><a href="<?=Url::to(['gallery/index'])?>">ГАЛЕРЕЯ</a></li>
-                    <?php if(\app\models\User::isVtcMember()): ?>
+                    <?php if(!\app\models\User::isVtcMember()): ?>
                         <li<?php if(Yii::$app->controller->action->id === 'recruit'){?> class="active"<?php } ?>><a href="<?=Url::to(['site/recruit'])?>">ВСТУПИТЬ</a></li>
                     <?php endif ?>
                     <li<?php if(Yii::$app->controller->id === 'mods'){?> class="active"<?php } ?>><a href="<?=Url::to(['modifications/index'])?>">МОДЫ</a></li>
