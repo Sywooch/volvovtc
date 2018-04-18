e460b02eb751f62259aa615ebbff74f7b9ba2cbe
viiper94 <viiper94@gmail.com>
2018-04-18 17:31:48 +0300

    removed some statics and optimizations

--- models/ClaimsRecruit.php
+++ models/ClaimsRecruit.php
@@ -39,16 +39,6 @@ class ClaimsRecruit extends Claims{
         ];
     }
 
-//    public function getStatusTitle(){
-//        switch ($this->status){
-//            case '1': return 'Одобрено'; break;
-//            case '2': return 'Отказ'; break;
-//            case '3': return 'На удержании'; break;
-//            case '0':
-//            default : return 'Рассматривается'; break;
-//        }
-//    }
-
 	public static function getClaims($limit = null){
 		$claims = ClaimsRecruit::find()
 			->select([
